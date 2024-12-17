d20c5760028da6a4600b28a0cce0a6a1ee032194
Ilya Morozov <morozov1308@yandex.ru>
2024-12-17 22:52:00 +0300

    Функция форматирования данных

--- WeatherNow.py
+++ WeatherNow.py
@@ -16,3 +16,16 @@ def get_weather(city: str, API_key: str) -> dict:
     response = requests.get(url)
     print(response.status_code)
     return response.json()
+
+def format_weather_message(weather_dict: dict) -> str:
+    """
+    Функция форматирования данных о погоде для читаемого вида
+     - weather_dict: dict: Словарь с данными о погоде.
+     - return: Форматированное сообщение о погоде.    
+    """
+    location = weather_dict['name']
+    temp = weather_dict['main']['temp']
+    feels_like = weather_dict['main']['feels_like']
+    description = weather_dict['weather'][0]['description']
+    return  f'Локация: {location}\nТемпература: {temp}°C\nОщущается как: {feels_like}°C\nОписание: {description}'
+
